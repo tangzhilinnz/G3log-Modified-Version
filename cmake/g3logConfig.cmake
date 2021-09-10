e20266d2ff392723d10f87e6f5ccd997a24b5184
tangzhilin <tangzhilinnz@gmail.com>
2021-09-10 23:57:38 +0800

    added comments

--- cmake/g3logConfig.cmake
+++ cmake/g3logConfig.cmake
@@ -15,6 +15,9 @@
 # * ``G3LOG_LIBRARIES`` -- raw cmake variable with library link line
 # * ``G3LOG_FOUND`` -- check if the lib was found without using the newer ``if(TARGET g3log)...``
 
+# FindPackageHandleStandardArgs
+# This module provides functions intended to be used in Find Modules implementing
+# find_package(<PackageName>) calls.
 include(FindPackageHandleStandardArgs)
 include(SelectLibraryConfigurations)
 
@@ -24,7 +27,13 @@ find_package(Threads REQUIRED)
 
 if (NOT TARGET g3log)
    include("${CMAKE_CURRENT_LIST_DIR}/g3logTargets.cmake")
-
+   
+   # get_target_property(<VAR> target property)
+   # Get a property from a target. The value of the property is stored in the 
+   # variable <VAR>.
+   # INTERFACE_INCLUDE_DIRECTORIES
+   # List of public include directories requirements for a library.
+   # 
    get_target_property(G3LOG_INCLUDE_DIR g3log INTERFACE_INCLUDE_DIRECTORIES)
    get_target_property(G3LOG_LIBRARY_DEBUG g3log IMPORTED_IMPLIB_DEBUG)
    
@@ -37,6 +46,19 @@ if (NOT TARGET g3log)
       get_target_property(G3LOG_LIBRARY_RELEASE g3log IMPORTED_LOCATION_RELEASE)
    endif ()
 
+   # select_library_configurations(basename)
+   # This macro takes a library base name as an argument, and will choose good
+   # values for the variables
+   # basename_LIBRARY
+   # basename_LIBRARIES
+   # basename_LIBRARY_DEBUG
+   # basename_LIBRARY_RELEASE
+   # depending on what has been found and set.
+   # If only basename_LIBRARY_RELEASE is defined, basename_LIBRARY will be set
+   # to the release value, and basename_LIBRARY_DEBUG will be set to 
+   # basename_LIBRARY_DEBUG-NOTFOUND. If only basename_LIBRARY_DEBUG is defined, 
+   # then basename_LIBRARY will take the debug value, and basename_LIBRARY_RELEASE
+   # will be set to basename_LIBRARY_RELEASE-NOTFOUND.
    select_library_configurations(G3LOG)
 
    if (G3LOG_LIBRARY)
@@ -48,6 +70,11 @@ if (NOT TARGET g3log)
 endif ()
 
 find_package_handle_standard_args(G3LOG REQUIRED_VARS G3LOG_INCLUDE_DIR G3LOG_LIBRARY)
+# mark_as_advanced
+# Sets the advanced/non-advanced state of the named cached variables.
+# An advanced variable will not be displayed in any of the cmake GUIs unless
+# the show advanced option is on. In script mode, the advanced/non-advanced
+# state has no effect.
 mark_as_advanced(G3LOG_INCLUDE_DIR G3LOG_LIBRARY)
 set(G3LOG_INCLUDE_DIRS ${G3LOG_INCLUDE_DIR})
 set(G3LOG_LIBRARIES ${G3LOG_LIBRARY})

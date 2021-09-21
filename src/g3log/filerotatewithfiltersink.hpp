c78fdc528886a8cb78819c7b5f2f29aa55d030f7
tangzhilin <tangzhilinnz@gmail.com>
2021-09-21 12:22:24 +0800

    deleted filerotatewithfiltersink.cpp and moved its content to filerotatewithfiltersink.hpp

--- src/g3log/filerotatewithfiltersink.hpp
+++ src/g3log/filerotatewithfiltersink.hpp
@@ -7,6 +7,7 @@
 #include <utility>
 #include <memory>
 #include <vector>
+#include <algorithm>
 
 /**
 * Wraps a LogRotate file logger. It only forwareds log LEVELS
@@ -20,28 +21,99 @@ namespace g3 {
       using IgnoreLogLevels = std::vector<LEVELS>;
       
    public:
+       // helper function to create an logging sink with filter
       static std::unique_ptr<FileRotateWithFilter>  CreateLogRotateWithFilter(
-         std::string filename, std::string directory, std::vector<LEVELS> filter);
+         std::string filename, std::string directory, std::vector<LEVELS> filter) {
+         auto fileRotateUptr = std::make_unique<FileRotateSink>(filename, directory);
+         return std::make_unique<FileRotateWithFilter>(std::move(fileRotateUptr), filter);
+      }
 
-      FileRotateWithFilter(FileRotateUptr logToFile, IgnoreLogLevels ignoreLevels);
-      virtual ~FileRotateWithFilter();
+      /// @param logToFile rotate file logging sink
+      /// @param removes all log entries with LEVELS in this filter
+      FileRotateWithFilter(FileRotateUptr logToFile, IgnoreLogLevels ignoreLevels)
+         : logger_(std::move(logToFile))
+         , filter_(std::move(ignoreLevels))
+      {}
+
+      virtual ~FileRotateWithFilter() {}
       
-      void setMaxArchiveLogCount(int max_size);
-      int getMaxArchiveLogCount();
-      void setMaxLogSize(int max_file_size);
-      int getMaxLogSize();
+      void setMaxArchiveLogCount(int max_size) {
+         logger_->setMaxArchiveLogCount(max_size);
+      }
 
-      void save(g3::LogMessageMover logEntry);
-      void setFlushPolicy(size_t flush_policy); // 0: never (system auto flush), 1 ... N: every n times
-      void flush();
+      int getMaxArchiveLogCount() {
+         return logger_->getMaxArchiveLogCount();
+      }
 
-      std::string changeLogFile(const std::string& log_directory);
-      std::string logFileName();
-      bool rotateLog();
-      void setLogSizeCounter();
+      void setMaxLogSize(int max_file_size) {
+         logger_->setMaxLogSize(max_file_size);
+      }
+
+      int getMaxLogSize() {
+         return logger_->getMaxLogSize();
+      }
+
+      /// @param logEntry saves log entry that are not in the filter
+      void save(g3::LogMessageMover logEntry) {
+         auto level = logEntry.get()._level;
+         bool isNotInFilter_ = (filter_.end() == std::find(filter_.begin(), filter_.end(), level));
+
+         if (isNotInFilter_) {
+            //logger_->save(logEntry.get().toString(_log_details_func));
+            logger_->fileWrite(logEntry);
+         }
+      }
+
+      /**
+       * Flush policy: Default is every single time (i.e. policy of 1).
+       *
+       * If the system logs A LOT then it is likely better to allow for the system to buffer and write
+       * all the entries at once.
+       *
+       * 0: System decides, potentially very long time
+       * 1....N: Flush logs every n entry
+       */
+      void setFlushPolicy(size_t flush_policy) {
+         logger_->setFlushPolicy(flush_policy);
+      }
       
-      void overrideLogDetails(LogMessage::LogDetailsFunc func);
-      void overrideLogHeader(const std::string& change);
+      /**
+       * Force flush of log entries. This should normally be policed with the @ref setFlushPolicy
+       * but is great for unit testing and if there are special circumstances where you want to see
+       * the logs faster than the flush_policy
+       */
+      void flush() {
+         logger_->flush();
+      }
+
+      std::string changeLogFile(const std::string& log_directory) {
+         return logger_->changeLogFile(log_directory);
+      }
+
+      /// @return the current filename
+      std::string logFileName() {
+         return logger_->logFileName();
+      }
+
+      bool rotateLog() {
+         return logger_->rotateLog();
+      }
+
+      void setLogSizeCounter() {
+         logger_->setLogSizeCounter();
+      }
+
+      /**
+       * Override the defualt log formatting.
+       * Please see https://github.com/KjellKod/g3log/API.markdown for more details
+       */
+      void overrideLogDetails(LogMessage::LogDetailsFunc func) {
+         logger_->overrideLogDetails(func);
+      }
+
+      void overrideLogHeader(const std::string& change) {
+         logger_->overrideLogHeader(change);
+      }
 
     private:
       FileRotateUptr logger_;
